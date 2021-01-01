d94b8e5e80c5fbff0a47ba5337a0956f9439fa68
chirchir12 <chirchir7370@gmail.com>
2021-01-01 11:13:03 +0300

    send simple  email

--- sendEmail.py
+++ sendEmail.py
@@ -1,7 +1,20 @@
 import os
+import smtplib
 from dotenv import load_dotenv
 load_dotenv()
 
 sender_mail = os.environ.get('MY_EMAIL')
-passord = os.environ.get('EMAIL_PASSWORD')
-print(passord)
+password = os.environ.get('EMAIL_PASSWORD')
+
+
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()  # ecrypt
+    smtp.ehlo()
+
+    smtp.login(sender_mail, password)
+    subject = 'Hello there!!!!!!!!'
+    body = 'this is your first email man'
+
+    msg = f'Subject:{subject}\n\n{body}'
+    smtp.sendmail(sender_mail, 'chirchir7370@gmail.com', msg)
