d57d8988149f9540644adbc668192f10d4d22680
chirchir12 <chirchir7370@gmail.com>
2021-01-01 11:47:49 +0300

    send single attachements

--- sendEmail.py
+++ sendEmail.py
@@ -1,5 +1,6 @@
 import os
 import smtplib
+import imghdr
 from email.message import EmailMessage
 from dotenv import load_dotenv
 load_dotenv()
@@ -11,8 +12,14 @@ msg = EmailMessage()
 msg['Subject'] = 'Hooray this is 2021'
 msg['From'] = sender_mail
 msg['To'] = 'chirchir7370@gmail.com'
-msg.set_content('how about dinner today at 5pm')
+msg.set_content('we have attachement')
 
+with open('sendemails/image1.PNG', 'rb') as f:
+    file_data = f.read()
+    file_type = imghdr.what(f.name)
+    file_name = f.name
+
+msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(sender_mail, password)
