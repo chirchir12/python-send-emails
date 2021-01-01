3608a99fa8796b63880471564a388391278a6657
chirchir12 <chirchir7370@gmail.com>
2021-01-01 11:32:17 +0300

    updated sendemail to and structure it with emailmessage class

--- sendEmail.py
+++ sendEmail.py
@@ -1,16 +1,19 @@
 import os
 import smtplib
+from email.message import EmailMessage
 from dotenv import load_dotenv
 load_dotenv()
 
 sender_mail = os.environ.get('MY_EMAIL')
 password = os.environ.get('EMAIL_PASSWORD')
 
+msg = EmailMessage()
+msg['Subject'] = 'Hooray this is 2021'
+msg['From'] = sender_mail
+msg['To'] = 'chirchir7370@gmail.com'
+msg.set_content('how about dinner today at 5pm')
+
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(sender_mail, password)
-    subject = 'Hello there!!!!!!!!'
-    body = 'this is your first email man'
-
-    msg = f'Subject:{subject}\n\n{body}'
-    smtp.sendmail(sender_mail, 'chirchir7370@gmail.com', msg)
+    smtp.send_message(msg)
